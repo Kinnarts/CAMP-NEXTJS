dae8655519d0100af9364ba3a83e159281e076ad
Anton Zhirkov <anton_zhirkov@epam.com>
2023-12-13 14:49:57 +0100

    live preview begin

--- app/flash-sale/[sku]/page.tsx
+++ app/flash-sale/[sku]/page.tsx
@@ -1,24 +1,34 @@
+"use client";
 import { Stack } from "@/lib/utils/contentstack-client";
 import ContentstackLivePreview from "@contentstack/live-preview-utils";
+import { useEffect, useState } from "react";
 
-export default async function FlashSale({
-  params,
-}: {
-  params: { sku: string };
-}) {
-  ContentstackLivePreview.init();
+export default function FlashSale({ params }: { params: { sku: string } }) {
+  ContentstackLivePreview.init({
+    enable: true,
+  });
+  const [getPage, setPage] = useState({} as any);
 
-  const page = await Stack.ContentType("sales")
-    .Query()
-    .toJSON()
-    .where("url", `/flash-sale/${params.sku}`)
-    .find()
-    .then((response) => response[0][0]);
+  async function fetchData() {
+    setPage(
+      await Stack.ContentType("sales")
+        .Query()
+        .toJSON()
+        .where("url", `/flash-sale/${params.sku}`)
+        .find()
+        .then((response) => response[0][0])
+    );
+  }
+  const { onEntryChange } = ContentstackLivePreview;
+
+  useEffect(() => {
+    onEntryChange(() => fetchData());
+  }, []);
 
   return (
     <>
       <div className="flex">Flash sale page</div>
-      {page.intro}
+      {getPage?.intro}
     </>
   );
 }
--- lib/utils/contentstack-client.ts
+++ lib/utils/contentstack-client.ts
@@ -1,11 +1,11 @@
 import Contentstack from "contentstack";
 
 export const Stack = Contentstack.Stack({
-  api_key: process.env.CONTENTSTACK_API_KEY!,
-  environment: process.env.CONTENTSTACK_ENVIRONMENT!,
-  delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN!,
+  api_key: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY!,
+  environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT!,
+  delivery_token: process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN!,
   live_preview: {
-    management_token: process.env.CONTENTSTACK_TOKEN!,
+    management_token: process.env.NEXT_PUBLIC_CONTENTSTACK_TOKEN!,
     enable: true,
     host: "eu-api.contentstack.io",
   },
